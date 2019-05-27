7815ccfa9ce88228194cdcb683234f8dfd1a9222
Kris de Rycke <derycke.kris@gmail.com>
2019-05-27 14:14:06 +0200

    add first lines of js and Doge pic

---
+++ doge.js
@@ -0,0 +1,20 @@
+document.getElementById("doge").addEventListener("mouseover", woof);
+document.getElementById("doge").addEventListener("click", picture);
+document.getElementById("doge").addEventListener("load", loadPage);
+
+function woof() {
+  document.getElementById("doge").innerHTML = "woof woof!";
+}
+
+// function picture() {
+//   document.getElementById("doge").innerHTML = "fff";
+// }
+
+// document
+//   .getElementById("doge")
+//   .addEventListener(onmouseover, function mouseOver() {
+//     document.getElementById("doge").innerHTML = "Woof woof!";
+//   });
+// function mouseOut() {
+//   document.getElementById("doge").innerHTML = "Doggo";
+// }
