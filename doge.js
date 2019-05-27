74d67f6ff2de0751fe7f4f25d9c1caedb9e9f95c
Kris de Rycke <derycke.kris@gmail.com>
2019-05-27 15:56:01 +0200

    set all functions workable

--- doge.js
+++ doge.js
@@ -1,6 +1,5 @@
 document.getElementById("doge").addEventListener("mouseover", woof);
 document.getElementById("doge").addEventListener("click", picture);
-document.getElementById("doge").addEventListener("load", loadPage);
 
 function woof() {
   document.getElementById("doge").innerHTML = "woof woof!";
@@ -13,16 +12,3 @@ function picture() {
 function loadPage() {
   document.getElementById("doge").innerHTML = "woof";
 }
-
-// function picture() {
-//   document.getElementById("doge").innerHTML = "fff";
-// }
-
-// document
-//   .getElementById("doge")
-//   .addEventListener(onmouseover, function mouseOver() {
-//     document.getElementById("doge").innerHTML = "Woof woof!";
-//   });
-// function mouseOut() {
-//   document.getElementById("doge").innerHTML = "Doggo";
-// }
